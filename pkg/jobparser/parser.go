5bb02968bb94b6c2dfc58f753ead12d7232250fd
Adrian Stobbe <adrian.stobbe@sap.com>
2022-05-12 15:28:48 +0200

    load csv complete

--- pkg/jobparser/parser.go
+++ pkg/jobparser/parser.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"io"
 	"log"
+	"strconv"
 	"strings"
 )
 
@@ -19,6 +20,8 @@ type Record struct {
 
 func ParsePodMemories(f io.Reader) []PodMemory {
 	csvReader := csv.NewReader(f)
+	csvReader.LazyQuotes = true
+
 	data, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
@@ -27,20 +30,33 @@ func ParsePodMemories(f io.Reader) []PodMemory {
 }
 
 func parse(records [][]string) []PodMemory {
+
 	header := records[0]
-	res := make([]PodMemory, 0, len(header))
-	for i, title := range header {
-		if i > 0 { // omit time column
-			name := extractPodName(title)
-			res = append(res, PodMemory{Name: name})
+	// omit time column
+	res := initNamesForPodMemories(header)
+	for timeIdx, line := range records {
+		if timeIdx > 0 { // omit header line
+			time := line[0]
+			for podIdx, strmem := range line[1:] {
+				mem, _ := strconv.ParseFloat(strmem, 64)
+				res[podIdx].Records = append(res[podIdx].Records, Record{Time: time, Usage: mem})
+			}
 		}
+
 	}
-	// for i, line := range records {
-	//     if i > 0 { // omit header line
-	// 	}
-	// }
 	return res
 
+}
+
+func initNamesForPodMemories(header []string) []PodMemory {
+	res := make([]PodMemory, 0, len(header)-1)
+	for i, title := range header {
+		if i > 0 {
+			name := extractPodName(title)
+			res = append(res, PodMemory{Name: name, Records: make([]Record, 0, 100)})
+		}
+	}
+	return res
 }
 
 func extractPodName(header string) string {
--- pkg/jobparser/parser_test.go
+++ pkg/jobparser/parser_test.go
@@ -8,8 +8,14 @@ import (
 )
 
 func TestLoadCsv(t *testing.T) {
-	file := strings.NewReader(`"Date","o10n-worker-l-jbmbp-qsjc6 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-f88p8-z9hwl worker (builtin:containers.memory.residentSetBytes) [B]"`)
+	file := strings.NewReader(`"Date","o10n-worker-l-jbmbp-qsjc6 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-f88p8-z9hwl worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-rhcpr-g2pjm worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-45pnm-ghd49 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-kls5k-tw7jj worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-qpv88-w69cl worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-ghvqt-klrmb worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-879vd-qp7s6 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-94qhk-vxdxz worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-wnlfd-dv594 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-7j7bg-gqzgl worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-c5cjd-x8l58 worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-zgqdd-h4fwl worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-5r744-sq5pn worker (builtin:containers.memory.residentSetBytes) [B]","o10n-worker-l-s4dtc-l6k47 worker (builtin:containers.memory.residentSetBytes) [B]"
+"2022-05-11 08:00:00",21571718826.5,3524325375.5,54965999615.5,,,,,,,16074645503.5,22920224426.5,,,,`)
 	podmemories := ParsePodMemories(file)
 	assert.Equal(t, "o10n-worker-l-jbmbp-qsjc6", podmemories[0].Name)
+	assert.Equal(t, "2022-05-11 08:00:00", podmemories[0].Records[0].Time)
+	assert.Equal(t, 21571718826.5, podmemories[0].Records[0].Usage)
+
+	assert.Equal(t, "2022-05-11 08:00:00", podmemories[1].Records[0].Time)
 	assert.Equal(t, "o10n-worker-l-f88p8-z9hwl", podmemories[1].Name)
+	assert.Equal(t, 3524325375.5, podmemories[1].Records[0].Usage)
 }
