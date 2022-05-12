d761e1344fba5117cb1b44118f9988449b3cbaca
Adrian Stobbe <adrian.stobbe@sap.com>
2022-05-12 16:32:18 +0200

    create pod

---
+++ pkg/jobparser/pod.go
@@ -0,0 +1,59 @@
+package jobparser
+
+import (
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func CreatePod(podinfo PodMemory) *v1.Pod {
+	simSpec := ""
+	cpu := "8" // s: 5-10; m: 8-10; l:8-10
+	startTime := podinfo.Records[0].Time
+	for _, record := range podinfo.Records {
+		time := record.Time.Sub(startTime).Seconds()
+		simSpec += fmt.Sprintf(`
+- seconds: %f
+  resourceUsage:
+    cpu: %s
+    memory: %f
+`, time, cpu, record.Usage)
+	}
+	pod := v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Pod",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podinfo.Name,
+			Namespace: "default",
+			Annotations: map[string]string{
+				"simSpec": simSpec,
+			},
+		},
+		// Spec: v1.PodSpec{
+
+		// 	Containers: []v1.Container{
+		// 		{
+		// 			Name:  "container",
+		// 			Image: "container",
+		// 			Resources: v1.ResourceRequirements{
+		// 				Requests: v1.ResourceList{
+		// 					"cpu":            resource.MustParse("4"),
+		// 					"memory":         resource.MustParse("4Gi"),
+		// 					"nvidia.com/gpu": resource.MustParse("1"),
+		// 				},
+		// 				Limits: v1.ResourceList{
+		// 					"cpu":            resource.MustParse("6"),
+		// 					"memory":         resource.MustParse("6Gi"),
+		// 					"nvidia.com/gpu": resource.MustParse("1"),
+		// 				},
+		// 			},
+		// 		},
+		// 	},
+		// },
+	}
+
+	return &pod
+}
--- pkg/jobparser/pod_test.go
+++ pkg/jobparser/pod_test.go
@@ -1,7 +1,14 @@
 package jobparser
 
-// func TestPodSpecFromPodMemory(t *testing.T) {
-// 	podmem := PodMemory{Name:"w1",Records: []Record{}}
-// 	podspec := CreatePod(podmem)
+import (
+	"testing"
+	"time"
 
-// }
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPodSpecFromPodMemory(t *testing.T) {
+	podmem := PodMemory{Name: "w1", Records: []Record{{Time: time.Now(), Usage: 1e9}, {Time: time.Now().Add(2 * time.Minute), Usage: 1e2}}}
+	podspec := CreatePod(podmem)
+	assert.Equal(t, "\n- seconds: 0.000000\n  resourceUsage:\n    cpu: 8\n    memory: 1000000000.000000\n\n- seconds: 120.000000\n  resourceUsage:\n    cpu: 8\n    memory: 100.000000\n", podspec.Annotations["simSpec"])
+}
