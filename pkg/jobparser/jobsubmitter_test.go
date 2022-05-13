b3b6572f6511e26c862565d2ac023075c16e01cf
Adrian Stobbe <adrian.stobbe@sap.com>
2022-05-13 11:05:37 +0200

    add test to remove processed jobs

--- pkg/jobparser/jobsubmitter_test.go
+++ pkg/jobparser/jobsubmitter_test.go
@@ -13,7 +13,7 @@ import (
 func TestSubmitJobWhenTime(t *testing.T) {
 	now := time.Now()
 
-	jobs := []jobparser.PodMemory{{Name: "j1", StartAt: now, Records: []jobparser.Record{{Time: now, Usage: 100.}}}}
+	jobs := []jobparser.PodMemory{{Name: "j1", StartAt: now, Records: []jobparser.Record{{Time: now, Usage: 100.}, {Time: now.Add(1 * time.Hour), Usage: 100.}}}}
 	t.Run("submit job when at exact same time", func(t *testing.T) {
 		sut := jobparser.NewJobSubmitter(jobs)
 		simTime := clock.NewClock(now)
@@ -44,6 +44,17 @@ func TestSubmitJobWhenTime(t *testing.T) {
 		// assertSubmitEvent(t, events[0], "j1")
 		// assertTerminateEvent(t, events[1])
 	})
+	t.Run("remove job from submitter once submitted", func(t *testing.T) {
+		sut := jobparser.NewJobSubmitter(jobs)
+		simTime := clock.NewClock(now)
+		_, err := sut.Submit(simTime, nil, nil)
+		assert.NoError(t, err)
+		events, err := sut.Submit(simTime, nil, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(events))
+		// assert.False(t, isTerminateEvent(events[0]))
+		// fmt.Printf("+%v", events)
+	})
 }
 
 func assertSubmitEvent(t testing.TB, event submitter.Event, podName string) {
@@ -53,6 +64,10 @@ func assertSubmitEvent(t testing.TB, event submitter.Event, podName string) {
 }
 
 func assertTerminateEvent(t testing.TB, event submitter.Event) {
-	_, ok := event.(*submitter.TerminateSubmitterEvent)
-	assert.True(t, ok)
+	assert.True(t, isTerminateEvent(event))
+}
+
+func isTerminateEvent(event submitter.Event) (ok bool) {
+	_, ok = event.(*submitter.TerminateSubmitterEvent)
+	return
 }
