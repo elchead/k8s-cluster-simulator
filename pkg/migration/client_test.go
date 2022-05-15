6566305285561aefb8f9b34b0e5a3ef7184afb1a
Adrian Stobbe <adrian.stobbe@sap.com>
2022-05-15 13:03:07 +0200

    create node for test

---
+++ pkg/migration/client_test.go
@@ -0,0 +1,30 @@
+package migration_test
+
+import (
+	"testing"
+
+	"time"
+
+	"github.com/elchead/k8s-cluster-simulator/pkg/clock"
+	"github.com/elchead/k8s-cluster-simulator/pkg/config"
+	"github.com/elchead/k8s-cluster-simulator/pkg/node"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+func TestCreateNode(t *testing.T) {
+	node,err := newNode("zone2","450Gi")
+	assert.NoError(t, err)
+	nodeInfo, _ := node.ToNodeInfo(clock.NewClock(time.Now()))
+	assert.Equal(t,int64(483183820800),nodeInfo.AllocatableResource().Memory)	
+}
+
+func newNode(name, memCapacity string) (node.Node,error) {
+	resources := map[v1.ResourceName]string{"memory": memCapacity}
+
+	nodeConfig := config.NodeConfig{Metadata:  metav1.ObjectMeta{Name:"zone2"},Spec: v1.NodeSpec{},Status:config.NodeStatus{Allocatable: resources}} 
+
+	v1Node, err := config.BuildNode(nodeConfig,"2022-05-11T08:00:00Z")
+
+	return node.NewNode(v1Node),err
+}
