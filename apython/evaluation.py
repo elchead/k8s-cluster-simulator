plot pods (incorrect values?)

apython/evaluation.py
@@ -3,6 +3,16 @@ from collections import defaultdict
 from dataclasses import dataclass
 from re import S
 from typing import List
+import numpy as np
+
+maxRestarts = 10
+
+
+def PodDataWith(time, memory):
+    p = PodData()
+    p.memory = memory
+    p.time = time
+    return p
 
 
 class PodData:
@@ -22,12 +32,18 @@ class Job:
     nodes: "dict[str,PodData]"
     name: str
     nbr_migrations: int
-    migration_order: "List[str]"
+    node_order: "List[str]"
 
     def __init__(self):
         self.nodes = defaultdict(PodData)
         self.nbr_migrations = 0
-        self.migration_order = ["", "", "", ""]
+        self.node_order = [""] * maxRestarts
+        self.node_data = [None] * maxRestarts
+
+    def get_pod_runs(self):
+        data = [e for e in self.node_data if e]
+        zones = [e for e in self.node_order if e != ""]
+        return zip(zones, data)
 
 
 def bytesto(bytes):
@@ -144,6 +160,7 @@ def merge_jobs(jobs):
     # check and count prepended m's
     new_jobs = defaultdict(Job)  # [job][node]
     for jobname, nodes in jobs.items():
+        count = 0
         if jobname.startswith("m"):
             count = count_m(jobname)
 
@@ -152,7 +169,7 @@ def merge_jobs(jobs):
                 if nodedata.memory:
                     old_node = list(jobs[jobname[1:]].keys())[0]
                     new_jobs[jobname[count:]].nodes[old_node].restored = True
-                new_jobs[jobname[count:]].migration_order[count] = node
+                new_jobs[jobname[count:]].node_order[count] = node
 
             jobname = jobname[count:]
             if count > new_jobs[jobname].nbr_migrations:
@@ -160,10 +177,28 @@ def merge_jobs(jobs):
 
         for node, nodedata in nodes.items():
             if not jobname.startswith("m"):
-                new_jobs[jobname].migration_order[0] = node
+                new_jobs[jobname].node_order[0] = node
             new_jobs[jobname].nodes[node].memory = nodedata.memory  # check if restarted on same node
             new_jobs[jobname].nodes[node].time = nodedata.time
+
+            new_jobs[jobname].node_data[count] = PodDataWith(
+                nodedata.time, nodedata.memory
+            )  # check if restarted on same node
+
     return new_jobs
+
+
+def adjust_time_stamps(jobs: "dict[str,Job]"):
+    for jobname, job in jobs.items():
+        for idx, data in enumerate(job.node_data[1:]):
+            if data:
+                last_time = jobs[jobname].node_data[idx].time[-1]
+                new_time = np.array(data.time) + last_time
+                data.time = new_time
+                if data.time[0] != 0:
+                    data.migration_idx = [0]
+
+    return jobs
 
 
 def count_m(job):