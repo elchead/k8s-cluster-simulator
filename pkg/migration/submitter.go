abebf2db2700dd522d81faff0f2d45a2351d0e69
Adrian Stobbe <adrian.stobbe@sap.com>
2022-07-05 14:04:40 +0200

    remove checker code from submitter

--- pkg/migration/submitter.go
+++ pkg/migration/submitter.go
@@ -2,7 +2,6 @@ package migration
 
 import (
 	"fmt"
-	"math"
 	"time"
 
 	"github.com/containerd/containerd/log"
@@ -18,116 +17,8 @@ import (
 	"k8s.io/kubernetes/pkg/scheduler/algorithm"
 )
 
-const MigrationTime = 5 * time.Minute
-const BackoffInterval = 45 * time.Second // TODO change to 30s? time to actually schedule pods on cluster
-type ControllerI interface {
-	GetMigrations() (migrations []migration.MigrationCmd, err error)
-}
-
-func GetMigrationTime(gbSz float64) time.Duration {
-	return time.Duration(math.Ceil(3.127*gbSz))*time.Second
-}
-
-func NewConcurrentMigrationChecker() *concurrentMigrationChecker {
-	return &concurrentMigrationChecker{make(map[string]clock.Clock),make(map[string]clock.Clock),nil}
-}
-
-type concurrentMigrationChecker struct {
-	migrationFinish map[string]clock.Clock
-	migrationStart map[string]clock.Clock
-	latestFinish *clock.Clock
-}
-func (m *concurrentMigrationChecker) StartMigration(t clock.Clock,gbSize float64,pod string) {
-	m.migrationStart[pod] = t
-	m.migrationFinish[pod] = m.getLastMigrationFinishTime(gbSize,t)
-}
-
-func (m *concurrentMigrationChecker) getLastMigrationFinishTime(gbSize float64,now clock.Clock) clock.Clock {
-	if m.latestFinish == nil {
-		m.latestFinish = &now
-	}
-
-	startTime := maxClock(*m.latestFinish, now)
-	res := startTime.Add(GetMigrationTime(gbSize))
-	m.latestFinish = &res
-	return res
-}
-
-func maxClock(t1, t2 clock.Clock) (startTime clock.Clock) {
-	if t2.BeforeOrEqual(t1) {
-		startTime = t1
-	} else {
-		startTime = t2
-	}
-	return
-}
-
-func (m *concurrentMigrationChecker) GetMigrationFinishTime(pod string) clock.Clock {
-	return m.migrationFinish[pod]
-} 
-
-func (m *concurrentMigrationChecker) IsReady(current clock.Clock) bool { return true }
-
-func NewMigrationChecker(checkerType string) MigrationCheckerI {
-	switch checkerType {
-	case "blocking":  return NewBlockingMigrationChecker()
-	case "concurrent": return NewConcurrentMigrationChecker()
-	default: 
-		log.L.Warnf("unsupported checker type %v; using blocking type", checkerType) 
-		return &blockingMigrationChecker{MigrationChecker{}}
-	}
-}
-type blockingMigrationChecker struct {
-	adapter MigrationChecker
-}
-
-func NewBlockingMigrationChecker() *blockingMigrationChecker {
-	return &blockingMigrationChecker{MigrationChecker{}}
-}
-
-func (m *blockingMigrationChecker) StartMigration(t clock.Clock,gbSize float64,pod string) {
-	m.adapter.StartMigrationWithSize(t,gbSize)	
-}
-
-func (m *blockingMigrationChecker) GetMigrationFinishTime(pod string) clock.Clock {
-	return m.adapter.GetMigrationFinishTime()
-} 
-
-func (m *blockingMigrationChecker) IsReady(current clock.Clock) bool { return m.adapter.IsReady(current) }
-
-
-type MigrationCheckerI interface {
-	StartMigration(t clock.Clock,gbSize float64,pod string)
-	GetMigrationFinishTime(pod string) clock.Clock
-	IsReady(current clock.Clock) bool
-}
-
-
-type MigrationChecker struct {
-	migrationStart clock.Clock
-	migrationDuration time.Duration
-}
-
-func (m *MigrationChecker) StartMigration(t clock.Clock) {
-	m.migrationStart = t
-	m.migrationDuration = MigrationTime
-}
-
-func (m *MigrationChecker) StartMigrationWithSize(t clock.Clock,gbSize float64)  {
-	m.migrationStart = t
-	m.migrationDuration = GetMigrationTime(gbSize)
-}
-
-func (m *MigrationChecker) GetMigrationFinishTime() clock.Clock {
-	return m.migrationStart.Add(m.migrationDuration)
-}
-
-func (m *MigrationChecker) IsReady(current clock.Clock) bool { return m.GetMigrationFinishTime().Add(BackoffInterval).BeforeOrEqual(current) } 
-
-
-
 type MigrationSubmitter struct {
-	controller ControllerI
+	controller monitoring.ControllerI
 	jobs []jobparser.PodMemory
 	queue jobparser.Iterator
 	endTime clock.Clock
@@ -215,19 +106,19 @@ func (m *MigrationSubmitter) startMigrations(migrations []migration.MigrationCmd
 	return nil
 }
 
-func NewSubmitter(controller ControllerI) *MigrationSubmitter {
+func NewSubmitter(controller monitoring.ControllerI) *MigrationSubmitter {
 	return &MigrationSubmitter{controller: controller}
 }
 
-func NewSubmitterWithJobs(controller ControllerI,jobs []jobparser.PodMemory) *MigrationSubmitter {
+func NewSubmitterWithJobs(controller  monitoring.ControllerI,jobs []jobparser.PodMemory) *MigrationSubmitter {
 	return &MigrationSubmitter{controller: controller,jobs: jobs,queue: *jobparser.NewIterator([]jobparser.PodMemory{})}
 }
 
-func NewSubmitterWithJobsWithEndTime(controller ControllerI,jobs []jobparser.PodMemory,endTime time.Time) *MigrationSubmitter {
+func NewSubmitterWithJobsWithEndTime(controller monitoring.ControllerI,jobs []jobparser.PodMemory,endTime time.Time) *MigrationSubmitter {
 	return &MigrationSubmitter{controller: controller,jobs: jobs,queue: *jobparser.NewIterator([]jobparser.PodMemory{}),endTime: clock.NewClock(endTime),factory: jobparser.PodFactory{SetResources:false},checker: monitoring.NewBlockingMigrationChecker()}
 }
 
-func NewSubmitterWithJobsWithEndTimeFactory(controller ControllerI,jobs []jobparser.PodMemory,endTime time.Time,factory jobparser.PodFactory,checker monitoring.MigrationCheckerI) *MigrationSubmitter {
+func NewSubmitterWithJobsWithEndTimeFactory(controller  monitoring.ControllerI,jobs []jobparser.PodMemory,endTime time.Time,factory jobparser.PodFactory,checker monitoring.MigrationCheckerI) *MigrationSubmitter {
 	return &MigrationSubmitter{controller: controller,jobs: jobs,queue: *jobparser.NewIterator([]jobparser.PodMemory{}),endTime: clock.NewClock(endTime),factory: factory,checker: checker}
 }
 
